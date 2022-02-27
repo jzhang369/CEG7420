fcd388d91296636f5203c0dc0554c3fb78827525
jzhang369 <junjie.gtisc@gmail.com>
2022-02-27 16:34:01 -0500

    added

--- temp/sscanf.c
+++ temp/sscanf.c
@@ -1,8 +1,41 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
 int main()
 {
+    char input[20];
+
+    int year; 
+    int day;
+    char month[20];
+
+    printf("Please enter a date (like - Feb 27 2022)\n");
+    fgets(input, sizeof(input), stdin); 
+    printf("your input is %s\n", input);
+
+    sscanf(input, "%s %d %d", month, &day, &year); 
+    printf("Year: %d\n", year); 
+    printf("Month: %s\n", month); 
+    printf("Day: %d\n", day); 
+
+    //this is the patched code:
+    /*
+    int ret = sscanf(input, "%s %d %d", month, &day, &year); 
+    if(ret == 3)
+    {
+        printf("ret saves the number of variables that are initialized.\n"); 
+        printf("Now it is (partially) OK to proceed.\n");
+        printf("Year: %d\n", year); 
+        printf("Month: %s\n", month); 
+        printf("Day: %d\n", day); 
+    }
+    else
+    {
+        printf("ERROR: the input does not have three elements.\n"); 
+    }
+    */
+
+
+
+    return 0;  
 
 }
